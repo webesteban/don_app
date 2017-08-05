020d0e176a57675c63849b3c50c295cff71aaac5
Daseram <daserami@gmail.com>
2017-08-04 22:54:18 -0500

    Adjusted date format

--- app/controllers/api/events_controller.rb
+++ app/controllers/api/events_controller.rb
@@ -10,9 +10,9 @@ class API::EventsController < API::APIController
                 lat: event.lat,
                 lon: event.lon,
                 address: event.address,
-                start_time: event.start_time,
+                start_time: event.start_time.strftime("%m/%d/%Y %H:%M") ,
                 id: event.id,
-                end_time: event.end_time
+                end_time: event.end_time.strftime("%m/%d/%Y %H:%M")
             })
         end
         response = {:events => json_events}
